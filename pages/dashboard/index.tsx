ac9a44f8475af1ff7f726bec3810ae985e7d0bc1
AndriiBoichuk <boichukandri9@gmail.com>
2022-09-30 16:46:01 +0300

    added redirections

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import { DashboardScreen } from '../../src/screens/Dashboard/DashboardScreen';
+
+const Dashboard: NextPage = () => {
+  return <DashboardScreen />;
+};
+
+export default Dashboard;
